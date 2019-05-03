475097cec3f899d43aedf298e3e61500da0ebeee
ScriptsEngineer <scriptsengineer@gmail.com>
2019-05-03 17:54:46 -0300

    Add connection type

--- SESServer/Server.cs
+++ SESServer/Server.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using SESockets;
 using SESockets.Utils;
+using SESockets.TCP;
+using SESockets.UDP;
 
 namespace SESockets.Server
 {
@@ -16,10 +18,41 @@ namespace SESockets.Server
 
         public Server()
         {
-            TCPServerComunicator server = new TCPServerComunicator();
+            Comunicator server;
+            ConnectionType type = ConnectionType();
+
+            Console.Clear();
+            Console.WriteLine("Connection type = " + type);
+
+            if (type == Utils.ConnectionType.TCP)
+            {
+                server = new ServerTCPComunicator();
+            }
+               
+            else //(type == Utils.ConnectionType.UDP)
+            {
+                server = new ServerUDPComunicator();
+            }
+
             server.SetWire(this);
             server.Connect(IPUtils.GetLocalIP());
+            
+
+            
         }
+
+        public ConnectionType ConnectionType()
+        {
+            Console.WriteLine("Connection Type:");
+            Console.WriteLine("1. TCP");
+            Console.WriteLine("2. UDP");
+            Console.Write("Enter to type server:");
+            int type = Convert.ToInt32(Console.ReadLine());
+            return type==1?Utils.ConnectionType.TCP:Utils.ConnectionType.UDP;
+        }
+
+        
+
 
 
         public void Log(string text)
---
+++ SESockets/Utils/ConnectionType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SESockets.Utils
+{
+    public enum ConnectionType
+    {
+
+        TCP,UDP
+
+    }
+}
