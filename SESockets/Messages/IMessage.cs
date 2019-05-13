6260d3af419c7d51790bf9202e9b17c1536a66a3
ScriptsEngineer <scriptsengineer@gmail.com>
2019-05-13 14:48:14 -0300

    Add Initial Messages

---
+++ SESockets/Messages/IMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SESockets.Message
+{
+    //Implementa um interface para mensagem enviadas de sockets
+    public interface IMessage
+    {
+
+        MessageType Type { get; }
+
+    }
+}
---
+++ SESockets/Messages/MessageType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SESockets.Messages
+{
+    public enum MessageType
+    {
+
+        CONNECT
+
+    }
+}
